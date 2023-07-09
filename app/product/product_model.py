e2fd7b505a93655cfa8d1d166e0fa42c3a9344a0
Tan Ke Wei <kwtan@outlook.sg>
2023-07-09 12:52:05 +0800

    Add product model

---
+++ app/product/product_model.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+from app.library.pydantic import PydanticObjectId
+from pydantic import BaseModel, Field
+
+
+class Product(BaseModel):
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
+    name: str
+    description: Optional[str] = None
+    price: float
+    is_deleted: bool = Field(default=False, alias="isDeleted")
+    created_at: datetime = Field(alias="createdAt")
