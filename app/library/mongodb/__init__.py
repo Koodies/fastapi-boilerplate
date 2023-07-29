48daf50a7c25af9f57a522f3f06d791247ef4e49
Tan Ke Wei <kwtan@outlook.sg>
2023-07-29 15:11:13 +0800

    MV mongo connection string to .env

--- app/library/mongodb/__init__.py
+++ app/library/mongodb/__init__.py
@@ -1,6 +1,8 @@
+import os
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from typing import Optional
+
 
 class Database:
     instance = None
@@ -8,12 +10,13 @@ class Database:
     def __new__(cls):
         if cls.instance is None:
             cls.instance = super().__new__(cls)
-            cls.instance.client = MongoClient("mongodb://localhost:27017/")
+            cls.instance.client = MongoClient(os.getenv("MONGO_CONNECTION_URI", None))
             cls.instance.db = cls.instance.client["test"]
         return cls.instance
-    
+
     def collection(self, collection_name) -> Collection:
         return self.instance.db[collection_name]
+
 
 def get_db() -> Optional[Database]:
     return Database()
